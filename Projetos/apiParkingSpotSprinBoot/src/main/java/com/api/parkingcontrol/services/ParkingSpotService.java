bc472f48ae6818d76a4014113baa4b1a709d7ed2
0Guilhermino <angeloguilhermino@gmail.com>
2023-01-04 14:42:53 -0300

    update certificados

--- Projetos/apiParkingSpotSprinBoot/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ Projetos/apiParkingSpotSprinBoot/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -48,6 +48,7 @@ public class ParkingSpotService {
 
     @Transactional
     public void delete(ParkingSpotModel parkingSpotModel) {
+
         parkingSpotRepository.delete(parkingSpotModel);
     }
 }
